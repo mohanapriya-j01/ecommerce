ec913bb6fc1f8e596b8ae36d57070d75c59a379c
mohanapriya-j01 <84305692+mohanapriya-j01@users.noreply.github.com>
2021-05-18 13:04:01 +0530

    Add files via upload

---
+++ OrderMS/src/main/java/com/infosys/order/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.infosys.order.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infosys.order.dto.OrderDTO;
+import com.infosys.order.entity.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+	public OrderDTO findByBuyerId(Integer buyerId);
+}
